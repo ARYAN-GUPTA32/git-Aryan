3bc1cbcb07bcdd99f04f277704461fc01799bba6
Aryan Gupta <anaryagu@gmail.com>
2025-09-17 09:41:51 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const age =24;
-const name ="aryan "
+const name1 ="aryan "
 const dob='01/01/2005'
 function sayName(){
-        console.log('My name is '+ name )
+        console.log('My name is '+ name1 )
+}
+function isAbove18(){
+    return age>=18;
 }
